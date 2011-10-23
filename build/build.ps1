ab23e65d93f58b41d952ea2dba38bf4f1675c1cd
Mikael Ostberg <me@mikeeast.it>
2011-10-24 00:18:30 +0200

    build testing

--- build/build.ps1
+++ build/build.ps1
@@ -6,7 +6,7 @@ Properties {
 	$tools_dir = "$code_dir\packages"
 	$nunit_dir = "$tools_dir\NUnit.2.5.10.11092\tools"
 	$env:path = "$env:path;$nunit_dir"
-	
+	$nunitlauncher=%teamcity.dotnet.nunitlauncher%
 }
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
@@ -19,7 +19,7 @@ Task Info {
 	Write-Host "Build dir: $build_dir" -ForegroundColor Green
 	Write-Host "Build output dir: $build_output_dir" -ForegroundColor Green
 	Write-Host "Code dir: $code_dir" -ForegroundColor Green
-	Write-Host "Nunit: ${global:teamcity.dotnet.nunitlauncher}" 
+	Write-Host "Nunit: ${global:teamcity.dotnet.nunitlauncher} $$nunitlauncher" 
 }
 
 Task Build -Depends Clean {	
